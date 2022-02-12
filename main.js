ab59070fb84512fefbe7ab168f22ddd4020a868c
Shinhee Park <shinhee.park777@gmail.com>
2022-02-11 17:38:51 -0800

    Hero edited

---
+++ main.js
@@ -0,0 +1,9 @@
+//Menu Toggle
+
+// function classToggle(){
+//     const navs = document.querySelectorAll('.myLinks')
+//     navs.forEach(nav => nav.classList.toggle('.myLinks'))
+// }
+
+// document.querySelector('#menu-open')
+//     .addEventListener('click', classToggle)
